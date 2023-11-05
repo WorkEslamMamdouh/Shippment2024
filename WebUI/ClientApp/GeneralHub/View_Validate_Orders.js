cad6c08edf74c02d98526fa09f1a19e2ae79d2cd
WorkEslamMamdouh <81823203+WorkEslamMamdouh@users.noreply.github.com>
2023-11-05 21:29:19 +0200

    Update

--- WebUI/ClientApp/GeneralHub/View_Validate_Orders.js
+++ WebUI/ClientApp/GeneralHub/View_Validate_Orders.js
@@ -10,24 +10,29 @@ var View_Validate_Orders;
     var _InvoiceItems = new Array();
     var txtSearch;
     var Filter_Select_Seller;
+    var Filter_View;
+    var btnDelete_Filter;
     function InitalizeComponent() {
         InitalizeControls();
         InitializeEvents();
         $('#Txt_From_Date').val(DateStartMonth());
         $('#Txt_To_Date').val(GetDate());
-        GetData_Invoice();
         InitializeGrid();
-        Display_Orders();
+        GetData_Invoice();
         Close_Loder();
     }
     View_Validate_Orders.InitalizeComponent = InitalizeComponent;
     function InitalizeControls() {
         txtSearch = document.getElementById('txtSearch');
         Filter_Select_Seller = document.getElementById('Filter_Select_Seller');
+        Filter_View = document.getElementById('Filter_View');
+        btnDelete_Filter = document.getElementById('btnDelete_Filter');
     }
     function InitializeEvents() {
         txtSearch.onkeyup = _SearchBox_Change;
         Filter_Select_Seller.onclick = Filter_Select_Seller_onclick;
+        Filter_View.onclick = function () { $('#btnDelete_Filter').removeClass('display_none'); GetData_Invoice(); };
+        btnDelete_Filter.onclick = Clear;
     }
     function InitializeGrid() {
         _Grid.ElementName = "_Grid";
@@ -53,8 +58,9 @@ var View_Validate_Orders;
                     return txt;
                 }
             },
-            { title: "Name", name: "CustomerName", type: "text", width: "100px" },
-            { title: "Mobile", name: "CustomerMobile1", type: "text", width: "100px" },
+            { title: "Comp Name", name: "REMARKS", type: "text", width: "100px" },
+            { title: "Vnd Name", name: "Vnd_Name", type: "text", width: "100px" },
+            { title: "Mobile", name: "Vnd_Mobile", type: "text", width: "100px" },
             { title: "ItemCount", name: "ItemCount", type: "number", width: "100px" },
             { title: "Total", name: "NetAfterVat", type: "text", width: "100px" },
         ];
@@ -74,24 +80,33 @@ var View_Validate_Orders;
         }
     }
     function GetData_Invoice() {
+        CleaningList_Table();
         debugger;
+        var StartDate = DateFormat($('#Txt_From_Date').val());
+        var EndDate = DateFormat($('#Txt_To_Date').val());
+        var Con = "";
+        if (Number($('#Txt_VendorID').val()) != 0) {
+            Con = " and VendorID =" + Number($('#Txt_VendorID').val());
+        }
         var Table;
         Table =
             [
-                { NameTable: 'Sls_Invoice', Condition: " TrType = 0 and Status = 1 " },
-                { NameTable: 'Sls_InvoiceItem', Condition: " InvoiceID in (Select InvoiceID from [dbo].[Sls_Invoice] where TrType = 0 and Status = 1" },
+                { NameTable: 'Vnd_Inv_SlsMan', Condition: " TrType = 0 and Status = 1 and TrDate >=N'" + StartDate + "' and TrDate <= N'" + EndDate + "'" + Con },
+                { NameTable: 'Sls_InvoiceItem', Condition: " InvoiceID in (Select InvoiceID from [dbo].[Sls_Invoice] where TrType = 0 and Status = 1 and TrDate >=N'" + StartDate + "' and TrDate <= N'" + EndDate + "' " + Con + ")" },
             ];
         DataResult(Table);
         //**************************************************************************************************************
         debugger;
-        _Invoices = GetDataTable('Sls_Invoice');
+        _Invoices = GetDataTable('Vnd_Inv_SlsMan');
         _InvoiceItems = GetDataTable('Sls_InvoiceItem');
+        _Invoices = _Invoices.sort(dynamicSort("InvoiceID"));
         SetGlopelDataInvoice(_Invoices);
         SetGlopelDataInvoiceItems(_InvoiceItems);
+        Display_Orders();
     }
     function Display_Orders() {
-        _Invoices = _Invoices.sort(dynamicSort("InvoiceID"));
-        _Grid.DataSource = _Invoices;
+        var _Invs = _Invoices;
+        _Grid.DataSource = _Invs;
         _Grid.Bind();
         $('#Txt_Total_LineCount').val(_Invoices.length);
         $('#Txt_Total_ItemsCount').val(SumValue(_Invoices, "ItemCount"));
@@ -99,9 +114,21 @@ var View_Validate_Orders;
     }
     function Filter_Select_Seller_onclick() {
         sys.FindKey("Select_Seller", "btnSelect_Seller", "", function () {
+            debugger;
+            var dataScr = SearchGrid.SearchDataGrid.dataScr;
             var id = SearchGrid.SearchDataGrid.SelectedKey;
-            alert(id);
+            dataScr = dataScr.filter(function (x) { return x.VendorID == id; });
+            $('#Txt_VendorID').val(id);
+            Filter_Select_Seller.innerHTML = "( " + dataScr[0].NAMEL + " )";
         });
+    }
+    function Clear() {
+        $('#Txt_From_Date').val(DateStartMonth());
+        $('#Txt_To_Date').val(GetDate());
+        $('#Txt_VendorID').val('');
+        Filter_Select_Seller.innerHTML = 'Select Seller';
+        $('#btnDelete_Filter').addClass('display_none');
+        GetData_Invoice();
     }
 })(View_Validate_Orders || (View_Validate_Orders = {}));
 //# sourceMappingURL=View_Validate_Orders.js.map
--- WebUI/ClientApp/GeneralHub/View_Validate_Orders.ts
+++ WebUI/ClientApp/GeneralHub/View_Validate_Orders.ts
@@ -9,11 +9,13 @@ namespace View_Validate_Orders {
     var SysSession: SystemSession = GetSystemSession();
     var _Grid: JsGrid = new JsGrid();
 
-    var _Invoices: Array<Sls_Invoice> = new Array <Sls_Invoice>();
+    var _Invoices: Array<Vnd_Inv_SlsMan> = new Array<Vnd_Inv_SlsMan>();
     var _InvoiceItems: Array<Sls_InvoiceItem> = new Array<Sls_InvoiceItem>();
 
     var txtSearch: HTMLInputElement;
     var Filter_Select_Seller: HTMLButtonElement;
+    var Filter_View: HTMLButtonElement;
+    var btnDelete_Filter: HTMLButtonElement;
      
     export function InitalizeComponent() {
 
@@ -21,20 +23,23 @@ namespace View_Validate_Orders {
         InitalizeControls();
         InitializeEvents();
         $('#Txt_From_Date').val(DateStartMonth())
-        $('#Txt_To_Date').val(GetDate())
+        $('#Txt_To_Date').val(GetDate()) 
+        InitializeGrid(); 
         GetData_Invoice();
-        InitializeGrid();
-        Display_Orders();
         Close_Loder(); 
     }
     function InitalizeControls() {
         txtSearch = document.getElementById('txtSearch') as HTMLInputElement;
         Filter_Select_Seller = document.getElementById('Filter_Select_Seller') as HTMLButtonElement;
+        Filter_View = document.getElementById('Filter_View') as HTMLButtonElement;
+        btnDelete_Filter = document.getElementById('btnDelete_Filter') as HTMLButtonElement;
     }
     function InitializeEvents() {
 
         txtSearch.onkeyup = _SearchBox_Change;
         Filter_Select_Seller.onclick = Filter_Select_Seller_onclick;
+        Filter_View.onclick = () => { $('#btnDelete_Filter').removeClass('display_none'); GetData_Invoice() };
+        btnDelete_Filter.onclick = Clear;
     }
     function InitializeGrid() { 
         _Grid.ElementName = "_Grid";
@@ -54,14 +59,15 @@ namespace View_Validate_Orders {
             { title: "RefNO", name: "RefNO", type: "number", width: "100px" },
             {
                 title: "TrDate", css: "ColumPadding", name: "TrDate", width: "100px",
-                itemTemplate: (s: string, item: Sls_Invoice): HTMLLabelElement => {
+                itemTemplate: (s: string, item: Vnd_Inv_SlsMan): HTMLLabelElement => {
                     let txt: HTMLLabelElement = document.createElement("label");
                     txt.innerHTML = DateFormat(item.TrDate);
                     return txt;
                 }
             },
-            { title: "Name", name: "CustomerName", type: "text", width: "100px" },
-            { title: "Mobile", name: "CustomerMobile1", type: "text", width: "100px" },
+            { title: "Comp Name", name: "REMARKS", type: "text", width: "100px" },
+            { title: "Vnd Name", name: "Vnd_Name", type: "text", width: "100px" }, 
+            { title: "Mobile", name: "Vnd_Mobile", type: "text", width: "100px" },
             { title: "ItemCount", name: "ItemCount", type: "number", width: "100px" },
             { title: "Total", name: "NetAfterVat", type: "text", width: "100px" },
             
@@ -84,42 +90,64 @@ namespace View_Validate_Orders {
         }
     }
     function GetData_Invoice() {
+        CleaningList_Table();
         debugger
-
+        let StartDate = DateFormat($('#Txt_From_Date').val());
+        let EndDate = DateFormat($('#Txt_To_Date').val());
+        let Con = "";
+        if (Number($('#Txt_VendorID').val()) != 0) {
+            Con = " and VendorID =" + Number($('#Txt_VendorID').val());
+        }
         var Table: Array<Table>;
         Table =
             [
-            { NameTable: 'Sls_Invoice', Condition: " TrType = 0 and Status = 1 " },
-            { NameTable: 'Sls_InvoiceItem', Condition: " InvoiceID in (Select InvoiceID from [dbo].[Sls_Invoice] where TrType = 0 and Status = 1" },
+                { NameTable: 'Vnd_Inv_SlsMan', Condition: " TrType = 0 and Status = 1 and TrDate >=N'" + StartDate + "' and TrDate <= N'" + EndDate + "'" + Con },
+                { NameTable: 'Sls_InvoiceItem', Condition: " InvoiceID in (Select InvoiceID from [dbo].[Sls_Invoice] where TrType = 0 and Status = 1 and TrDate >=N'" + StartDate + "' and TrDate <= N'" + EndDate + "' " + Con + ")" },
             ]
 
         DataResult(Table);
         //**************************************************************************************************************
         debugger
-        _Invoices = GetDataTable('Sls_Invoice');
+        _Invoices = GetDataTable('Vnd_Inv_SlsMan');
         _InvoiceItems = GetDataTable('Sls_InvoiceItem');
+
+        _Invoices = _Invoices.sort(dynamicSort("InvoiceID"));
 
         SetGlopelDataInvoice(_Invoices);
         SetGlopelDataInvoiceItems(_InvoiceItems);
-        
-    } 
+
+        Display_Orders();
+
+    }
     function Display_Orders() {
-        
-        _Invoices = _Invoices.sort(dynamicSort("InvoiceID")); 
-        _Grid.DataSource = _Invoices;
+
+        let _Invs = _Invoices
+        _Grid.DataSource = _Invs;
         _Grid.Bind();
-         
+
 
         $('#Txt_Total_LineCount').val(_Invoices.length);
         $('#Txt_Total_ItemsCount').val(SumValue(_Invoices, "ItemCount"));
         $('#Txt_Total_Amount').val(SumValue(_Invoices, "NetAfterVat", 1));
-    } 
+    }
 
     function Filter_Select_Seller_onclick() {
         sys.FindKey("Select_Seller", "btnSelect_Seller", "", () => {
+            debugger
+            let dataScr = SearchGrid.SearchDataGrid.dataScr
             let id = SearchGrid.SearchDataGrid.SelectedKey
-            alert(id);
+            dataScr = dataScr.filter(x => x.VendorID == id);
+            $('#Txt_VendorID').val(id)
+            Filter_Select_Seller.innerHTML = "( " + dataScr[0].NAMEL + " )"; 
         });
+    }
+    function Clear() {
+        $('#Txt_From_Date').val(DateStartMonth())
+        $('#Txt_To_Date').val(GetDate())
+        $('#Txt_VendorID').val('')
+        Filter_Select_Seller.innerHTML = 'Select Seller'
+        $('#btnDelete_Filter').addClass('display_none')
+        GetData_Invoice();
     }
      
 }
