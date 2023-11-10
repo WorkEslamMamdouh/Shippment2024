ccac2df86b5d94e5105d42468abfc41928b92f6c
WorkEslamMamdouh <81823203+WorkEslamMamdouh@users.noreply.github.com>
2023-11-11 00:11:00 +0200

    update

--- WebUI/ClientApp/Order/Edit_Order.js
+++ WebUI/ClientApp/Order/Edit_Order.js
@@ -5,7 +5,12 @@ var Edit_Order;
 (function (Edit_Order) {
     var sys = new SystemTools();
     var SysSession = GetSystemSession();
+    var _USER = new Array();
     var InvMasterDetails = new InvoiceMasterDetails();
+    var _Inv = new Vnd_Inv_SlsMan();
+    var _Invoices = new Array();
+    var _InvoiceItems = new Array();
+    var _InvItems = new Array();
     var Id_Back;
     var Id_Next;
     var Id_Back_Step2;
@@ -15,13 +20,26 @@ var Edit_Order;
     var Txt_Quantity;
     var Txt_UnitPrice;
     var Txt_NetTotal;
+    var Txt_PrcVatAmount;
+    var Txt_CommitionAmount;
     var CountGrid = 0;
     var NumItems = 0;
+    var ItemTotal = 0;
+    var ItemCount = 0;
+    var InvoiceID = 0;
     function InitalizeComponent() {
+        var _USERS = GetGlopelDataUser();
+        _USER = _USERS.filter(function (x) { return x.USER_CODE == SysSession.CurrentEnvironment.UserCode; });
+        InitalizeControls();
+        InitializeEvents();
+        Clear();
+        _Invoices = GetGlopelDataInvoice();
+        _InvoiceItems = GetGlopelDataInvoiceItems();
+        InvoiceID = Number(localStorage.getItem("InvoiceID"));
+        _Inv = _Invoices.filter(function (x) { return x.InvoiceID == InvoiceID; })[0];
+        _InvItems = _InvoiceItems.filter(function (x) { return x.InvoiceID == InvoiceID; });
+        Display_information_Inv();
         Close_Loder();
-        //InitalizeControls();
-        //InitializeEvents();
-        //Clear();
     }
     Edit_Order.InitalizeComponent = InitalizeComponent;
     function InitalizeControls() {
@@ -34,6 +52,8 @@ var Edit_Order;
         Txt_Quantity = document.getElementById("Txt_Quantity");
         Txt_UnitPrice = document.getElementById("Txt_UnitPrice");
         Txt_NetTotal = document.getElementById("Txt_NetTotal");
+        Txt_PrcVatAmount = document.getElementById("Txt_PrcVatAmount");
+        Txt_CommitionAmount = document.getElementById("Txt_CommitionAmount");
     }
     function InitializeEvents() {
         Id_Next.onclick = _Next;
@@ -44,6 +64,8 @@ var Edit_Order;
         Btn_AddItem.onclick = AddItemBox;
         Txt_UnitPrice.onkeyup = BoxTotal;
         Txt_Quantity.onkeyup = BoxTotal;
+        Txt_PrcVatAmount.onkeyup = TotalComplet;
+        Txt_CommitionAmount.onkeyup = TotalComplet;
     }
     function _Next() {
         if (!Valid_Header()) {
@@ -89,8 +111,14 @@ var Edit_Order;
         $('#Div_Review_invoice').removeClass('display_none');
     }
     function _Finish() {
+        debugger;
+        if (!$('.User1').is(":hidden")) {
+            if (!Valid_Input("Txt_CommitionAmount", "Please a Enter Commition Amount 😡", "1")) {
+                return false;
+            }
+        }
         Assign();
-        Insert();
+        Update();
     }
     //****************************************************** Validtion and Clear *****************************************
     function Valid_Header() {
@@ -158,7 +186,7 @@ var Edit_Order;
         if (!Valid_Item()) {
             return;
         }
-        BuildBox(CountGrid, $('#Txt_Name_Item').val(), $('#Txt_UnitPrice').val(), $('#Txt_Quantity').val(), 0, 0, SysSession.CurrentEnvironment.VendorID);
+        BuildBox(CountGrid, $('#Txt_Name_Item').val(), $('#Txt_UnitPrice').val(), $('#Txt_Quantity').val(), 0, 0, _Inv.VendorID);
         $('#StatusFlag' + CountGrid).val("i");
         CountGrid++;
         NumItems++;
@@ -199,11 +227,22 @@ var Edit_Order;
         Header.CustomerMobile2 = $('#Txt_Phone_Num2').val().trim();
         Header.Address = $('#Txt_Address1').val().trim();
         Header.Location = $('#Txt_location').val().trim();
-        Header.TrDate = DateFormatRep($('#Txt_Receive_TrData').val());
+        Header.TrDate = DateFormatRep($('#Txt__TrData').val());
+        Header.DeliveryDate = DateFormatRep($('#Txt_Receive_TrData').val());
         Header.PromoCode = $('#txt_Promo_Code').val().trim();
         Header.UserCode = SysSession.CurrentEnvironment.UserCode;
-        Header.VendorID = Model[0].VendorID;
+        Header.VendorID = _Inv.VendorID;
+        Header.VatType = Number($('#Txt_PrcVatAmount').val());
+        Header.VatAmount = Number($('#Txt_VatAmount').val());
+        Header.TotalAmount = Number($('#Txt_TotalAmount').val());
+        Header.CommitionAmount = Number($('#Txt_CommitionAmount').val());
+        Header.NetAfterVat = Number($('#Txt_NetAmount').val());
+        Header.ItemCount = Number(ItemCount);
         Header.TrType = 0;
+        Header.CompCode = 1;
+        Header.BranchCode = 1;
+        Header.UpdatedBy = SysSession.CurrentEnvironment.UserCode;
+        Header.UpdatedAt = GetDate();
         InvMasterDetails.Sls_InvoiceItem = Model;
         InvMasterDetails.Sls_Invoice = Header;
         InvMasterDetails.Branch_Code = SysSession.CurrentEnvironment.BranchCode;
@@ -212,12 +251,12 @@ var Edit_Order;
         InvMasterDetails.UserCode = SysSession.CurrentEnvironment.UserCode;
         InvMasterDetails.sec_FinYear = SysSession.CurrentEnvironment.CurrentYear;
     }
-    function Insert() {
+    function Update() {
         console.log(InvMasterDetails);
         try {
-            Ajax.Callsync({
+            Ajax.CallsyncSave({
                 type: "Post",
-                url: sys.apiUrl("SlsInvoice", "Insert"),
+                url: sys.apiUrl("SlsInvoice", "Update"),
                 data: JSON.stringify(InvMasterDetails),
                 success: function (d) {
                     debugger;
@@ -228,6 +267,7 @@ var Edit_Order;
                         ShowMessage("Inserted 😍");
                         _Back();
                         Clear();
+                        Close_Loder();
                     }
                     else {
                         ShowMessage("Error 😒");
@@ -246,15 +286,59 @@ var Edit_Order;
         $('#Tran_ID_Print').html("<strong>Transaction ID:</strong> " + $('#Txt_Ref_No').val());
         $('#Tran_Date_Print').html("<strong>Date:</strong> " + $('#Txt_Receive_TrData').val());
         $('#Body_Inv_Print').html('');
-        var ItemTotal = 0;
+        ItemTotal = 0;
+        ItemCount = 0;
         for (var i = 0; i < CountGrid; i++) {
             if ($('#StatusFlag' + i).val() != 'd' && $('#StatusFlag' + i).val() != 'm') {
-                var Row = "    <tr>\n                            <td>" + $("#NameItem" + i).val() + "</td>\n                            <td>" + $("#SoldQty" + i).val() + "</td>\n                            <td>" + $("#Unitprice" + i).val() + "</td>\n                            <td>" + $("#ItemTotal" + i).val() + "</td>\n                        </tr>";
+                var Row = "    <tr>\n                            <td>" + $("#ItemDescA" + i).val() + "</td>\n                            <td>" + $("#SoldQty" + i).val() + "</td>\n                            <td>" + $("#Unitprice" + i).val() + "</td>\n                            <td>" + $("#ItemTotal" + i).val() + "</td>\n                        </tr>";
                 ItemTotal = ItemTotal + Number($("#ItemTotal" + i).val());
+                ItemCount = ItemCount + Number($("#SoldQty" + i).val());
                 $('#Body_Inv_Print').append(Row);
             }
         }
+        $('#Txt_TotalAmount').val(ItemTotal.toFixed(2));
+        $('#Txt_NetAmount').val(ItemTotal.toFixed(2));
         $('#Total_inv_Print').html(ItemTotal.toFixed(2));
+    }
+    function Display_information_Inv() {
+        debugger;
+        $('#Txt_InvoiceID').val(_Inv.InvoiceID);
+        $('#Txt_Ref_No').val(_Inv.RefNO);
+        $('#Txt_Name_Cust').val(_Inv.CustomerName);
+        $('#Txt_Phone_Num1').val(_Inv.CustomerMobile1);
+        $('#Txt_Phone_Num2').val(_Inv.CustomerMobile2);
+        $('#Txt_Address1').val(_Inv.Address);
+        $('#Txt_location').val(_Inv.Location);
+        $('#Txt_Remarks').val(_Inv.Remark);
+        $('#Txt_Receive_TrData').val(DateFormat(_Inv.DeliveryDate));
+        $('#Txt__TrData').val(DateFormat(_Inv.TrDate));
+        $('#txt_Promo_Code').val(_Inv.PromoCode);
+        $('#txt_VendorID').val(_Inv.VendorID);
+        $('#Txt_PrcVatAmount').val(_Inv.VatType);
+        $('#Txt_VatAmount').val(_Inv.VatAmount);
+        $('#Txt_TotalAmount').val(_Inv.TotalAmount);
+        $('#Txt_CommitionAmount').val(_Inv.CommitionAmount);
+        $('#Txt_NetAmount').val(_Inv.NetAfterVat);
+        Display_BuildBox();
+        TotalComplet();
+        $(".User" + _USER[0].USER_TYPE).removeClass('display_none');
+    }
+    function Display_BuildBox() {
+        for (var i = 0; i < _InvItems.length; i++) {
+            BuildBox(CountGrid, _InvItems[i].ItemDescA, _InvItems[i].Unitprice, _InvItems[i].SoldQty, _InvItems[i].InvoiceItemID, _InvItems[i].InvoiceID, Number(_Inv.VendorID));
+            $('#StatusFlag' + CountGrid).val("");
+            CountGrid++;
+            NumItems++;
+            $("#Tap_Reviews_Items").html("Reviews Items ( " + NumItems + " )");
+        }
+    }
+    function TotalComplet() {
+        var PrcVat = Number($('#Txt_PrcVatAmount').val());
+        var TotalAmount = Number($('#Txt_TotalAmount').val());
+        var VatAmount = ((PrcVat * TotalAmount) / 100);
+        $('#Txt_VatAmount').val(VatAmount);
+        var CommitionAmount = Number($('#Txt_CommitionAmount').val());
+        $('#Txt_NetAmount').val((VatAmount + TotalAmount + CommitionAmount).toFixed(2));
     }
 })(Edit_Order || (Edit_Order = {}));
 //# sourceMappingURL=Edit_Order.js.map
